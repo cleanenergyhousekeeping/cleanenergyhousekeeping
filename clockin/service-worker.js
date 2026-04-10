94639e68785efd471fd7cb6339cca5a2a350216d
cleanenergyhousekeeping <kyle@cleanenergyhousekeeping.com>
2026-04-10 11:45:23 -0700

    Create service-worker.js

---
+++ clockin/service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "ce-clockin-shell-v1";
+
+const APP_SHELL_FILES = [
+  "/clockin/",
+  "/clockin/index.html",
+  "/clockin/styles.css",
+  "/clockin/app.js",
+  "/clockin/manifest.webmanifest",
+  "/clockin/icon-192.png",
+  "/clockin/icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(request).catch(function () {
+      return caches.match(request).then(function (cachedResponse) {
+        return cachedResponse || caches.match("/clockin/index.html");
+      });
+    })
+  );
+});
