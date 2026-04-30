64e8b9ed1a35dbea3984948ce3c1e91376cf7db0
cleanenergyhousekeeping <kyle@cleanenergyhousekeeping.com>
2026-04-30 14:41:44 -0700

    Tighten shell HUD semantics and bump service worker cache (#2)

--- clockin/app.js
+++ clockin/app.js
@@ -1335,6 +1335,10 @@ async function syncShellQueue_() {
     }
 
     if (queueRemaining > 0) {
+      showShellFlashHud_("Saved on phone. Not synced yet.", false);
+    }
+
+    if (queueRemaining > 0) {
       setTimeout(function () {
         retryQueuedSyncIfReady_();
       }, 1500);
